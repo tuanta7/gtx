a7cfb66465e1836b24d7b73e8bdf3f890b31a957
tuanta7 <anhtuan9702@gmail.com>
2026-05-24 15:07:58 +0700

    chore: comment out toggle flag in root command

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,8 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func getOrInitGitHubClient() *auth.GitHubClient {
